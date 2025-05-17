70a60cc49ae3e66f8010576c28a2ef2929f6b6a2
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2025-05-17 10:57:25 -0300

    Importando Bibliotecas

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+# Permite acessar variáveis de ambiente do sistema e arquivos( como o .env)
+
+import requests
+# Biblioteca usada para fazer requisições HTTP (chamadas de API)
+
+from dotenv import load_dotenv
+# Carrega variáveis do arquivo ".env" para o ambiente Python.
+
+import time
+# Para evitar exxeceder o limite da API.
